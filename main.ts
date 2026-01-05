314e77afa2e1dc01e8096f429324011797f2d45d
214L <Aierc_W@hotmail.com>
2026-01-05 15:07:51 +0800

    chore(deps) : 移除不必要的 @leafer-in/find 依赖
    pick() 方法是 leafer-ui 核心功能，不需要额外安装 @leafer-in/find

--- main.ts
+++ main.ts
@@ -2,7 +2,6 @@ import { App, Ellipse, Rect } from 'leafer-ui'
 
 import { TooltipPlugin } from './src' // 引入插件代码
 import { Flow } from '@leafer-in/flow'
-import "@leafer-in/find"
 const app = new App({ view: window, tree: {} })
 
 const rect1 = new Rect({
